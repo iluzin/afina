c3419802815c156ad1f9b035fa7daf8ddf7241f4
Игорь <isluzin@mail.ru>
2017-10-04 23:53:32 +0300

    2-storage

--- include/afina/allocator/Simple.h
+++ include/afina/allocator/Simple.h
@@ -3,6 +3,7 @@
 
 #include <string>
 #include <cstddef>
+#include <cstring>
 
 namespace Afina {
 namespace Allocator {
--- src/storage/MapBasedGlobalLockImpl.cpp
+++ src/storage/MapBasedGlobalLockImpl.cpp
@@ -1,16 +1,118 @@
 #include "MapBasedGlobalLockImpl.h"
 
+#include <mutex>
+
 namespace Afina {
 namespace Backend {
 
 // See MapBasedGlobalLockImpl.h
-const std::string &MapBasedGlobalLockImpl::Put(const std::string &key, const std::string &value) { return ""; }
+bool MapBasedGlobalLockImpl::Put(const std::string &key, const std::string &value) {
+    std::unique_lock<std::mutex> guard(_lock);
+    if (_max_size < 1) {
+        return false;
+    }
+    if (_backend.empty()) {
+        _head = key;
+    } else if (_backend.size() == _max_size) {
+        std::string next = _backend[_head].second.second;
+        auto it = _backend.find(next);
+        if (it != _backend.end()) {
+            it->second.second.first = std::string();
+        }
+        _backend.erase(_head);
+        _head = next;
+    }
+    _backend[key].first = value;
+    _backend[key].second.first = _tail;
+    _tail = key;
+    return true;
+}
 
 // See MapBasedGlobalLockImpl.h
-const std::string &MapBasedGlobalLockImpl::PutIfAbsent(const std::string &key, const std::string &value) { return ""; }
+bool MapBasedGlobalLockImpl::PutIfAbsent(const std::string &key, const std::string &value) {
+    std::unique_lock<std::mutex> guard(_lock);
+    auto it = _backend.find(key);
+    if (it == _backend.end()) {
+        if (_max_size < 1) {
+            return false;
+        }
+        if (_backend.empty()) {
+            _head = key;
+        } else if (_backend.size() == _max_size) {
+            std::string next = _backend[_head].second.second;
+            it = _backend.find(next);
+            if (it != _backend.end()) {
+                it->second.second.first = std::string();
+            }
+            _backend.erase(_head);
+            _head = next;
+        }
+        _backend[key].first = value;
+        _backend[key].second.first = _tail;
+        _tail = key;
+        return true;
+    }
+    return false;
+}
 
 // See MapBasedGlobalLockImpl.h
-bool MapBasedGlobalLockImpl::Get(const std::string &key, std::string &value) { return false; }
+bool MapBasedGlobalLockImpl::Set(const std::string &key, const std::string &value) {
+    std::unique_lock<std::mutex> guard(_lock);
+    auto it = _backend.find(key);
+    if (it == _backend.end()) {
+        return false;
+    }
+    it->second.first = value;
+    if (_tail != key) {
+        std::string next = it->second.second.second;
+        it = _backend.find(it->second.second.first);
+        if (it != _backend.end()) {
+            it->second.second.second = next;
+        }
+        _backend[next].second.first = it->first;
+        _backend[_tail].second.second = key;
+        _backend[key].second.first = _tail;
+        _tail = key;
+    }
+    return true;
+}
+
+// See MapBasedGlobalLockImpl.h
+bool MapBasedGlobalLockImpl::Delete(const std::string &key) {
+    std::unique_lock<std::mutex> guard(_lock);
+    auto it = _backend.find(key);
+    if (it == _backend.end()) {
+        return false;
+    }
+    std::string next = it->second.second.second, prev = it->second.second.first;
+    _backend.erase(it);
+    it = _backend.find(prev);
+    if (it != _backend.end()) {
+        it->second.second.second = next;
+    }
+    it = _backend.find(next);
+    if (it != _backend.end()) {
+        it->second.second.first = prev;
+    }
+    if (_head == key) {
+        _head = next;
+    }
+    if (_tail == key) {
+        _tail = prev;
+    }
+    return true;
+}
+
+// See MapBasedGlobalLockImpl.h
+bool MapBasedGlobalLockImpl::Get(const std::string &key, std::string &value) const {
+    std::unique_lock<std::mutex> guard(*const_cast<std::mutex *>(&_lock));
+    auto it = _backend.find(key);
+    if (it == _backend.end()) {
+        return false;
+    }
+    value = it->second.first;
+    return true;
+}
 
 } // namespace MapBasedGlobalLockImpl
 } // namespace Afina
